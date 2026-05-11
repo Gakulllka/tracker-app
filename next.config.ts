eaa2cffce5cc375984fd90c45f01b1a5a58501a3
Fyodor <thenybas89@gmail.com>
2026-05-11 21:35:35 +0300

    Excel

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: ".",
-  },
-  output: "standalone",
   generateEtags: false,
   typescript: {
     ignoreBuildErrors: true,
